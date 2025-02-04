a23ef56544cfbcb2d8bc8fe5e2e1de9e985b3764
NaingPhonePyae <naingphonepyae@my.jcu.edu.au>
2025-02-04 15:27:58 +0800

    Refactor score program to use functions

--- prac_02/score.py
+++ prac_02/score.py
@@ -1,15 +1,26 @@
 """
-The intention is that the score must be between 0 and 100 inclusive; 90 or more is excellent; 50 or more is a pass;
-below 50 is bad. There is no intention to do any repetition.
+Result determining
 """
 
-score = float(input("Enter score: "))
-if score < 0 or score > 100:
-    result = "Invalid score"
-elif score >= 90:
-    result = "Excellent"
-elif score >= 50:
-    result = "Passable"
-else:
-    result = "Bad"
-print(result)
+
+def main():
+    """Result determining program"""
+    score = float(input("Enter score: "))
+    result = determine_grade(score)
+    print(result)
+
+
+def determine_grade(score):
+    """Determine grade based on score"""
+    if score < 0 or score > 100:
+        result = "Invalid score"
+    elif score >= 90:
+        result = "Excellent"
+    elif score >= 50:
+        result = "Passable"
+    else:
+        result = "Bad"
+    return result
+
+
+main()
