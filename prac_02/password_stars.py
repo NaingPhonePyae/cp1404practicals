8a83ac1122d4082abe3fb62fb2385941335cd277
NaingPhonePyae <naingphonepyae@my.jcu.edu.au>
2025-02-04 14:22:01 +0800

    Refactor get_password function

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -3,12 +3,17 @@ MINIMUM_LENGTH = 3
 
 
 def main():
+    password = get_password()
+    for i in range(len(password)):
+        print("*", end="")
+
+
+def get_password():
     password = input("Password: ")
     while len(password) < MINIMUM_LENGTH:
         print("Invalid")
         password = input("Password: ")
-    for i in range(len(password)):
-        print("*", end="")
+    return password
 
 
 main()
