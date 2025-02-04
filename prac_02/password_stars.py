4111fd41ff741e97fbb3d5f4f43a545525503aa4
NaingPhonePyae <naingphonepyae@my.jcu.edu.au>
2025-02-04 14:17:45 +0800

    Put code into main function

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -1,9 +1,14 @@
 """password stars"""
 MINIMUM_LENGTH = 3
 
-password = input("Password: ")
-while len(password) < MINIMUM_LENGTH:
-    print("Invalid")
+
+def main():
     password = input("Password: ")
-for i in range(len(password)):
-    print("*", end="")
+    while len(password) < MINIMUM_LENGTH:
+        print("Invalid")
+        password = input("Password: ")
+    for i in range(len(password)):
+        print("*", end="")
+
+
+main()
