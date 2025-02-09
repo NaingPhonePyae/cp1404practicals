e5594f4b96e237a62eb6f65afb063e00d7966d8e
NaingPhonePyae <naingphonepyae@my.jcu.edu.au>
2025-02-09 15:12:00 +0800

    Set the magic numbers as constant

--- prac_02/score.py
+++ prac_02/score.py
@@ -4,6 +4,11 @@ Result determining
 
 import random
 
+MAXIMUM_SCORE = 100
+MINIMUM_SCORE = 0
+EXCELLENT_SCORE = 90
+PASSABLE_SCORE = 50
+
 
 def main():
     """Result determining program"""
@@ -14,16 +19,16 @@ def main():
 
 def display_random_score():
     """Generates a random score between 0 and 100 and prints the result"""
-    print(f"{determine_grade(float(random.randint(0, 100)))}")
+    print(f"{determine_grade(float(random.randint(MINIMUM_SCORE, MAXIMUM_SCORE)))}")
 
 
 def determine_grade(score):
     """Determine grade based on score"""
-    if score < 0 or score > 100:
+    if score < MINIMUM_SCORE or score > MAXIMUM_SCORE:
         result = "Invalid score"
-    elif score >= 90:
+    elif score >= EXCELLENT_SCORE:
         result = "Excellent"
-    elif score >= 50:
+    elif score >= PASSABLE_SCORE:
         result = "Passable"
     else:
         result = "Bad"
