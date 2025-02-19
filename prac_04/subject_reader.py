064cbc9e2228bf0d89a3acf50adec06bce8b36e2
NaingPhonePyae <naingphonepyae@my.jcu.edu.au>
2025-02-19 15:09:21 +0800

    Complete tasks for subject_reader.py file

--- prac_04/subject_reader.py
+++ prac_04/subject_reader.py
@@ -7,13 +7,19 @@ FILENAME = "subject_data.txt"
 
 
 def main():
+    """print class details from file program"""
     data = load_data()
-    print(data)
+    for detail in data:
+        name_format = max([len(detail[1]) for detail in data])
+        student_format = max(len(str(detail[2])) for detail in data)
+        print(f"{detail[0]} is taught by {(detail[1]):<{name_format}} and has {(detail[2]):>{student_format}} "
+              f"students")
 
 
 def load_data():
     """Read data from file formatted like: subject,lecturer,number of students."""
-    input_file = open(FILENAME)
+    data = []
+    input_file = open(FILENAME, "r")
     for line in input_file:
         print(line)  # See what a line looks like
         print(repr(line))  # See what a line really looks like
@@ -22,8 +28,10 @@ def load_data():
         print(parts)  # See what the parts look like (notice the integer is a string)
         parts[2] = int(parts[2])  # Make the number an integer (ignore PyCharm's warning)
         print(parts)  # See if that worked
+        data.append([part for part in parts])  # Modified part
         print("----------")
     input_file.close()
+    return data
 
 
 main()
