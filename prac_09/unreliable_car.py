eba5322aa6aa291819fe02f9eaf43f60ebe4cc64
NaingPhonePyae <naingphonepyae@my.jcu.edu.au>
2025-04-13 22:14:49 +0800

    Complete tasks for unreliable_car.py

---
+++ prac_09/unreliable_car.py
@@ -0,0 +1,19 @@
+from prac_09.car import Car
+import random
+
+
+class UnreliableCar(Car):
+    """Specialised version of a Car that includes reliability."""
+
+    def __init__(self, name, fuel, reliability):
+        """Initialise a UnreliableCar instance, based on parent class Car."""
+        super().__init__(name, fuel)
+        self.reliability = reliability
+
+    def drive(self, distance):
+        """Drive like parent Car but based on reliability."""
+        random_number = self.reliability
+        if random_number <= random.randint(1, 100):
+            distance = 0
+        distance_driven = super().drive(distance)
+        return distance_driven
